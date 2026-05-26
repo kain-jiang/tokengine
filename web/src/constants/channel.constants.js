0f1f8afcb474197ce6207c5b67c0ee82b2fed926
Ruanxx053 <1787091339@qq.com>
2026-05-26 14:18:01 +0800

    feat-jingwei2

--- web/src/constants/channel.constants.js
+++ web/src/constants/channel.constants.js
@@ -189,6 +189,11 @@ export const CHANNEL_OPTIONS = [
     color: 'blue',
     label: 'Codex (OpenAI OAuth)',
   },
+  {
+    value: 58,
+    color: 'purple',
+    label: '鲸纬渠道',
+  },
 ];
 
 // Channel types that support upstream model list fetching in UI.
