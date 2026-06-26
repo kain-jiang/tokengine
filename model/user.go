0b325ccfe13a38a2402601dc6cf66d20b70ab1cd
yangxin <nameyangxin@163.com>
2026-06-26 16:32:49 +0800

    feat: 对接阿里实名认证

--- model/user.go
+++ model/user.go
@@ -52,7 +52,7 @@ type User struct {
 	Setting          string         `json:"setting" gorm:"type:text;column:setting"`
 	Remark           string         `json:"remark,omitempty" gorm:"type:varchar(255)" validate:"max=255"`
 	StripeCustomer   string         `json:"stripe_customer" gorm:"type:varchar(64);column:stripe_customer;index"`
-	TelePhone        string         `json:"telephone" gorm:"type:varchar(11);column:telephone;unique"` // 手机号
+	TelePhone        *string        `json:"telephone" gorm:"type:varchar(11);column:telephone;unique"` // 手机号
 }
 
 func (user *User) ToBaseUser() *UserBase {
