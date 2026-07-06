8e0bde46806973d397d460b6aa65bf6033de709b
yangxin <nameyangxin@163.com>
2026-07-06 13:53:38 +0800

    fix: 允许用户手机号为空

--- controller/user.go
+++ controller/user.go
@@ -216,11 +216,11 @@ func Register(c *gin.Context) {
 		}
 	}
 	// 验证短信验证码
-	if !common.VerifySMSCodeWithKey(user.TelePhone, user.VerificationCode) {
+	if !common.VerifySMSCodeWithKey(*user.TelePhone, user.VerificationCode) {
 		common.ApiErrorMsg(c, i18n.MsgUserVerificationCodeError)
 		return
 	}
-	common.DeleteSMSCode(user.TelePhone)
+	common.DeleteSMSCode(*user.TelePhone)
 	exist, err := model.CheckUserExistOrDeleted(user.Username, user.Email)
 	if err != nil {
 		common.ApiErrorI18n(c, i18n.MsgDatabaseError)
@@ -1126,7 +1126,7 @@ func PhoneBind(c *gin.Context) {
 		common.ApiError(c, err)
 		return
 	}
-	user.TelePhone = req.Telephone
+	user.TelePhone = &req.Telephone
 	// 绑定手机号
 	err = user.Update(false)
 	if err != nil {
