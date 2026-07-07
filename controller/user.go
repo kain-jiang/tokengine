062f9f63c93add4fa6a8b914508d098e50f130cd
yangxin <nameyangxin@163.com>
2026-07-07 14:41:33 +0800

    merge: 合并代码

--- controller/user.go
+++ controller/user.go
@@ -226,7 +226,7 @@ func Register(c *gin.Context) {
 		return
 	}
 	common.DeleteSMSCode(*user.TelePhone)
-	exist, err := model.CheckUserExistOrDeleted(user.Username, user.Email)
+	exist, err := model.CheckUserExistOrDeleted(user.Username, user.Email, *user.TelePhone)
 	if err != nil {
 		common.ApiErrorI18n(c, i18n.MsgDatabaseError)
 		common.SysLog(fmt.Sprintf("CheckUserExistOrDeleted error: %v", err))
@@ -1390,7 +1390,6 @@ func UpdateUserSetting(c *gin.Context) {
 
 	// 更新用户设置
 	user.SetSetting(settings)
-	fmt.Println(settings, "-------------")
 	if err := user.Update(false); err != nil {
 		common.ApiErrorI18n(c, i18n.MsgUpdateFailed)
 		return
